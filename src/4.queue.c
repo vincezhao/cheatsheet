1da6dea6e59cb34e21f8899e46d25b15e9bea4d1
Vince Zhao <vince.zhaowenxuan@gmail.com>
2013-10-17 00:37:36 -0300

    Add queue using circular array implementation

--- src/4.queue.c
+++ src/4.queue.c
@@ -1,1 +1,24 @@
+TYPE q[SIZE], data;
+int bq = 0, eq = 0;
 
+#define cnext(x) ((x+1) % SIZE)
+#define csize() ((eq + SIZE - bq) % SIZE)
+
+/* size */
+csize()
+
+/* empty */
+(0 == csize())
+(bq == eq)
+
+/* full */
+(SIZE-1 == csize())
+(cnext(eq) == bq)
+
+/* enqueue */
+q[eq] = data;
+eq = cnext(eq);
+
+/* dequeue */
+data = q[bq];
+bq = cnext(bq);
